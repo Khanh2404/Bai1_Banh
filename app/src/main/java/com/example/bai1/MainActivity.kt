bb3a5a030345835589c3ba0f893d9bae7befac24
khanh <vuvankhanh24042003@gmail.com>
2024-08-19 10:17:59 +0700

    capnhatmoi

--- app/src/main/java/com/example/bai1/MainActivity.kt
+++ app/src/main/java/com/example/bai1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.bai1
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,7 +11,7 @@ import androidx.core.view.WindowInsetsCompat
 
 class Cake(var imageCakeId:Int,var price: String, var quantity: Int)
 class MainActivity : AppCompatActivity() {
-    private lateinit var imageView: ImageView
+    private var imageView: ImageView? = null
     private lateinit var priceTextView: TextView
     private lateinit var quantityTextView:TextView
     private lateinit var chooseCakeTextView: TextView
@@ -25,13 +26,14 @@ class MainActivity : AppCompatActivity() {
     private var demSoLuongMatHang =0
     private var demSoTien = 0
     private fun updetaCakeUI(cake: Cake){
-        imageView.setImageResource(cake.imageCakeId)
+        imageView?.setImageResource(cake.imageCakeId)
         priceTextView.text=cake.price
         quantityTextView.text=cake.quantity.toString()
     }
     private fun cnSlmhSt( ){
         item.text="ban đang có $demSoLuongMatHang mặt hang";
         TongTien.text="Tổng Tiền : ${demSoTien}"
+        quantityTextView.text="${Cake[currentCakeIndex].quantity}";
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,7 +45,6 @@ class MainActivity : AppCompatActivity() {
         chooseCakeTextView=findViewById(R.id.txtCloss)
         item=findViewById(R.id.txtItems)
         TongTien=findViewById(R.id.txtTongTien)
-        var imageBanh = findViewById<ImageView>(R.id.imgBanh)
         updetaCakeUI(Cake[currentCakeIndex])
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -56,11 +57,17 @@ class MainActivity : AppCompatActivity() {
                 currentCakeIndex=0
             }
             updetaCakeUI(Cake[currentCakeIndex])
+            imageView?.isEnabled = Cake[currentCakeIndex].quantity > 0
         }
-        imageBanh.setOnClickListener{
+        imageView?.setOnClickListener{
             demSoLuongMatHang++
             var giaBanh = Cake[currentCakeIndex].price.replace("$","").toInt()
             demSoTien +=giaBanh
+            Cake[currentCakeIndex].quantity--
+            if (Cake[currentCakeIndex].quantity==0){
+                imageView?.isEnabled = false
+                Toast.makeText(this, "Hết bánh", Toast.LENGTH_SHORT).show()
+            }
             cnSlmhSt()
         }
     }
